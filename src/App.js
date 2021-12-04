be4917703e5c336d567279b96bfa316c26ff4e54
QuocEran <85829481+QuocEran@users.noreply.github.com>
2021-12-04 12:33:21 +0700

    morning_0412_Eran

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import "./App.css";
 import Header from "./Components/Header";
+import Home from "./Components/Home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     // BEM
-    <div className="App">
-      <Header />
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path="/">
+            <Header />
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Header.css";
 import SearchIcon from "@material-ui/icons/Search";
+import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
 
 function Header() {
   return (
@@ -18,18 +19,21 @@ function Header() {
 
       <div className="header__nav">
         <div className="header__options">
-          <span className="header__optionsLineOne">Hello Guest</span>
-          <span className="header__optionsLineTwo">Sign In</span>
+          <span className="header__optionLineOne">Hello Guest</span>
+          <span className="header__optionLineTwo">Sign In</span>
         </div>
         <div className="header__options">
-          <span className="header__optionsLineOne">Returns</span>
-          <span className="header__optionsLineTwo">& Order</span>
+          <span className="header__optionLineOne">Returns</span>
+          <span className="header__optionLineTwo">& Order</span>
         </div>
         <div className="header__options">
-          <span className="header__optionsLineOne">Your</span>
-          <span className="header__optionsLineTwo">Prime</span>
+          <span className="header__optionLineOne">Your</span>
+          <span className="header__optionLineTwo">Prime</span>
         </div>
-        <div className="header__options"></div>
+        <div className="header__optionBasket">
+          <ShoppingBasketIcon />
+          <span className="header__optionLineTwo header__basketCount">0</span>
+        </div>
       </div>
     </div>
   );
