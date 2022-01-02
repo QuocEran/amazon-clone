54b08c843f0e3190914a50b1b38bdc0adba58297
QuocEran <quocchuong45@gmail.com>
2022-01-02 18:03:59 +0700

    afternoon_02/01/22_Eran

--- src/Pages/Orders/Orders.js
+++ src/Pages/Orders/Orders.js
@@ -1,9 +1,39 @@
 import React from "react";
+import Order from "../../Components/Order";
+import { useEffect, useState } from "react";
+import { useStateValue } from "../../Store/StateProvider";
+import { projectFirestore } from "../../Configs/firebase";
 import "./Orders.css";
 function Orders() {
+  // eslint-disable-next-line no-unused-vars
+  const [{ basket, user }, dispatch] = useStateValue();
+  const [orders, setOrders] = useState([]);
+
+  useEffect(() => {
+    if (user) {
+      projectFirestore
+        .collection("users")
+        .doc(user?.uid)
+        .collection("orders")
+        .orderBy("created", "desc")
+        .onSnapshot((snapshot) =>
+          setOrders(
+            snapshot.docs.map((doc) => ({ id: doc.id, data: doc.data() }))
+          )
+        );
+    } else {
+      setOrders([]);
+    }
+  }, [user]);
+
   return (
     <div className="orders">
       <h1>Your Orders</h1>
+      <div className="orders__order">
+        {orders?.map((order) => (
+          <Order order={order} />
+        ))}
+      </div>
     </div>
   );
 }
